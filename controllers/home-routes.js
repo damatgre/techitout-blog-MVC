8bf93fdd3e0c6f76137aa90b2b1fc817c442f608
damatgre <gregdamato2@gmail.com>
2021-07-20 22:03:11 -0400

    create new-post and adjust stylesheet

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,6 +33,7 @@ router.get('/', (req, res) => {
     });
 });
 
+//check to see if logged in with session
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -41,9 +42,11 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+//signup route
 router.get('/signup', (req, res) => {
   res.render('signup');
 });
+
 
 router.get('/post/:id', (req, res) => {
   Post.findOne({
