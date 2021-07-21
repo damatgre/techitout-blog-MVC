2d8f34b7c73dd942d6257ecbbd6698e7426a4fa6
damatgre <gregdamato2@gmail.com>
2021-07-20 22:19:28 -0400

    fixed login/signup route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,11 +42,6 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-//signup route
-router.get('/signup', (req, res) => {
-  res.render('signup');
-});
-
 
 router.get('/post/:id', (req, res) => {
   Post.findOne({
